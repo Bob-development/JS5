3a96efc09e16d216b970e45ed1877b9797ddface
Bob-development <proinvoke228@gmail.com>
2023-07-10 18:41:35 +0300

    a

--- main.js
+++ main.js
@@ -1,143 +1,143 @@
 'use strict'
 // TASK 1
-// let isRunningConvertor = true;
-// let userTransferOfCarrency;
-// let userQuantityOfMoney;
+let isRunningConvertor = true;
+let userTransferOfCarrency;
+let userQuantityOfMoney;
 
-// while(isRunningConvertor){
-//   const userCarrency = prompt("Choose ur carrency:\n" + "a)Dollar;   " + "b)Euro;   " + "c)Funt;   " + "d)Yen;   ");
-//   userQuantityOfMoney = parseInt(prompt("How much money do u have?"));
+while(isRunningConvertor){
+  const userCarrency = prompt("Choose ur carrency:\n" + "a)Dollar;   " + "b)Euro;   " + "c)Funt;   " + "d)Yen;   ");
+  userQuantityOfMoney = parseInt(prompt("How much money do u have?"));
   
-//   switch (userCarrency.toLowerCase()) {
-//     case "a":
-//       userTransferOfCarrency = prompt("In which currency we would transfer?\n" + "a)Euro;   " + "b)Funt;   " + "c)Yen;   ");
-//       dollarTransfer();
-//       anotherTransfer();
-//       break;
+  switch (userCarrency.toLowerCase()) {
+    case "a":
+      userTransferOfCarrency = prompt("In which currency we would transfer?\n" + "a)Euro;   " + "b)Funt;   " + "c)Yen;   ");
+      dollarTransfer();
+      anotherTransfer();
+      break;
       
-//       case "b":
-//         userTransferOfCarrency = prompt("In which currency we would transfer?\n" + "a)Dollar;   " + "b)Funt;   " + "c)Yen;   ");
-//         euroTransfer();
-//         anotherTransfer()
-//         break;
+      case "b":
+        userTransferOfCarrency = prompt("In which currency we would transfer?\n" + "a)Dollar;   " + "b)Funt;   " + "c)Yen;   ");
+        euroTransfer();
+        anotherTransfer()
+        break;
     
-//     case "c":
-//       userTransferOfCarrency = prompt("In which currency we would transfer?\n" + "a)Dollar;   " + "b)Euro;   " + "c)Yen;   ");
-//       funtTransfer();
-//       anotherTransfer()
-//       break;
+    case "c":
+      userTransferOfCarrency = prompt("In which currency we would transfer?\n" + "a)Dollar;   " + "b)Euro;   " + "c)Yen;   ");
+      funtTransfer();
+      anotherTransfer()
+      break;
 
-//     case "d":
-//       userTransferOfCarrency = prompt("In which currency we would transfer?\n" + "a)Dollar;   " + "b)Euro;   " + "c)Funt;   ");
-//       yenTransfer();
-//       anotherTransfer()
-//       break;
+    case "d":
+      userTransferOfCarrency = prompt("In which currency we would transfer?\n" + "a)Dollar;   " + "b)Euro;   " + "c)Funt;   ");
+      yenTransfer();
+      anotherTransfer()
+      break;
 
-//     default:
-//       alert("Choose corect value...");
-//       break;
-//   }
-// }
+    default:
+      alert("Choose corect value...");
+      break;
+  }
+}
 
-// function dollarTransfer() {
-//   switch (userTransferOfCarrency.toLowerCase()) {
-//     case "a":
-//       let dollarResultOfTransferEuro = userQuantityOfMoney * 0.92;
-//       alert("Ur result: " + dollarResultOfTransferEuro.toFixed(2));
-//       break;
+function dollarTransfer() {
+  switch (userTransferOfCarrency.toLowerCase()) {
+    case "a":
+      let dollarResultOfTransferEuro = userQuantityOfMoney * 0.92;
+      alert("Ur result: " + dollarResultOfTransferEuro.toFixed(2));
+      break;
 
-//     case "b":
-//       let dollarResultOfTransferFunt = userQuantityOfMoney * 0.79;
-//       alert("Ur result: " + dollarResultOfTransferFunt.toFixed(2));
-//       break;
+    case "b":
+      let dollarResultOfTransferFunt = userQuantityOfMoney * 0.79;
+      alert("Ur result: " + dollarResultOfTransferFunt.toFixed(2));
+      break;
     
-//     case "c":
-//       let dollarResultOfTransferYen = userQuantityOfMoney * 144.30;
-//       alert("Ur result: " + dollarResultOfTransferYen.toFixed(2));
-//       break;
+    case "c":
+      let dollarResultOfTransferYen = userQuantityOfMoney * 144.30;
+      alert("Ur result: " + dollarResultOfTransferYen.toFixed(2));
+      break;
   
-//     default:
-//       alert("Choose corect value...");
-//       break;
-//   }
-// }
+    default:
+      alert("Choose corect value...");
+      break;
+  }
+}
 
-// function euroTransfer() {
-//   switch (userTransferOfCarrency.toLowerCase()) {
-//     case "a":
-//       let euroResultOfTransferDollar = userQuantityOfMoney * 1.09;
-//       alert("Ur result: " + euroResultOfTransferDollar.toFixed(2));
-//       break;
+function euroTransfer() {
+  switch (userTransferOfCarrency.toLowerCase()) {
+    case "a":
+      let euroResultOfTransferDollar = userQuantityOfMoney * 1.09;
+      alert("Ur result: " + euroResultOfTransferDollar.toFixed(2));
+      break;
 
-//     case "b":
-//       let euroResultOfTransferFunt = userQuantityOfMoney * 0.86;
-//       alert("Ur result: " + euroResultOfTransferFunt.toFixed(2));
-//       break;
+    case "b":
+      let euroResultOfTransferFunt = userQuantityOfMoney * 0.86;
+      alert("Ur result: " + euroResultOfTransferFunt.toFixed(2));
+      break;
     
-//     case "c":
-//       let euroResultOfTransferYen = userQuantityOfMoney * 157.62;
-//       alert("Ur result: " + euroResultOfTransferYen.toFixed(2));
-//       break;
+    case "c":
+      let euroResultOfTransferYen = userQuantityOfMoney * 157.62;
+      alert("Ur result: " + euroResultOfTransferYen.toFixed(2));
+      break;
   
-//     default:
-//       alert("Choose corect value...");
-//       break;
-//   }
-// }
+    default:
+      alert("Choose corect value...");
+      break;
+  }
+}
 
-// function funtTransfer() {
-//   switch (userTransferOfCarrency.toLowerCase()) {
-//     case "a":
-//       let funtResultOfTransferDollar = userQuantityOfMoney * 1.27;
-//       alert("Ur result: " + funtResultOfTransferDollar.toFixed(2));
-//       break;
+function funtTransfer() {
+  switch (userTransferOfCarrency.toLowerCase()) {
+    case "a":
+      let funtResultOfTransferDollar = userQuantityOfMoney * 1.27;
+      alert("Ur result: " + funtResultOfTransferDollar.toFixed(2));
+      break;
 
-//     case "b":
-//       let funtResultOfTransferEuro = userQuantityOfMoney * 1.16;
-//       alert("Ur result: " + funtResultOfTransferEuro.toFixed(2));
-//       break;
+    case "b":
+      let funtResultOfTransferEuro = userQuantityOfMoney * 1.16;
+      alert("Ur result: " + funtResultOfTransferEuro.toFixed(2));
+      break;
     
-//     case "c":
-//       let funtResultOfTransferYen = userQuantityOfMoney * 183.36;
-//       alert("Ur result: " + funtResultOfTransferYen.toFixed(2));
-//       break;
+    case "c":
+      let funtResultOfTransferYen = userQuantityOfMoney * 183.36;
+      alert("Ur result: " + funtResultOfTransferYen.toFixed(2));
+      break;
   
-//     default:
-//       alert("Choose corect value...");
-//       break;
-//   }
-// }
+    default:
+      alert("Choose corect value...");
+      break;
+  }
+}
 
-// function yenTransfer() {
-//   switch (userTransferOfCarrency.toLowerCase()) {
-//     case "a":
-//       let yenResultOfTransferDollar = userQuantityOfMoney * 0.0069;
-//       alert("Ur result: " + yenResultOfTransferDollar.toFixed(2));
-//       break;
+function yenTransfer() {
+  switch (userTransferOfCarrency.toLowerCase()) {
+    case "a":
+      let yenResultOfTransferDollar = userQuantityOfMoney * 0.0069;
+      alert("Ur result: " + yenResultOfTransferDollar.toFixed(2));
+      break;
 
-//     case "b":
-//       let yenResultOfTransferEuro = userQuantityOfMoney * 0.0063;
-//       alert("Ur result: " + yenResultOfTransferEuro.toFixed(2));
-//       break;
+    case "b":
+      let yenResultOfTransferEuro = userQuantityOfMoney * 0.0063;
+      alert("Ur result: " + yenResultOfTransferEuro.toFixed(2));
+      break;
     
-//     case "c":
-//       let yenResultOfTransferFunt = userQuantityOfMoney * 0.0055;
-//       alert("Ur result: " + yenResultOfTransferFunt.toFixed(2));
-//       break;
+    case "c":
+      let yenResultOfTransferFunt = userQuantityOfMoney * 0.0055;
+      alert("Ur result: " + yenResultOfTransferFunt.toFixed(2));
+      break;
   
-//     default:
-//       alert("Choose corect value...");
-//       break;
-//   }
-// }
+    default:
+      alert("Choose corect value...");
+      break;
+  }
+}
 
-// function anotherTransfer() {
-//   let transferAgain = prompt("Do u wanna transfer another currency?")
+function anotherTransfer() {
+  let transferAgain = prompt("Do u wanna transfer another currency?")
 
-//   if(transferAgain[0].toLowerCase() === 'n') isRunningConvertor = false;
-// }
+  if(transferAgain[0].toLowerCase() === 'n') isRunningConvertor = false;
+}
 
-// TASK 2
+TASK 2
 const me = {
 
 }
